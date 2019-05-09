21545c9a1e8332fa685c90f2821ec19aa8ca13f3
z.xin <z.xin@WKS-XIN>
2019-05-09 14:36:19 +0200

    callResourceServer1Hello works

--- resource-server2-feign/src/main/java/at/johnny/security/resourceserver2/feign/ResourceServer1FeignClient.java
+++ resource-server2-feign/src/main/java/at/johnny/security/resourceserver2/feign/ResourceServer1FeignClient.java
@@ -3,8 +3,8 @@ package at.johnny.security.resourceserver2.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="localFeignClient", url="http://localhost:8072")
-public interface LocalFeignClient {
+@FeignClient(name="resourceServer1Feign", url="http://localhost:8071")
+public interface ResourceServer1FeignClient {
     @GetMapping("/hello")
-    String callLocalHello();
+    public String callResourceServer1Hello();
 }
--- resource-server2-feign/src/main/java/at/johnny/security/resourceserver2/restcontroller/FeignController.java
+++ resource-server2-feign/src/main/java/at/johnny/security/resourceserver2/restcontroller/FeignController.java
@@ -1,6 +1,7 @@
 package at.johnny.security.resourceserver2.restcontroller;
 
 import at.johnny.security.resourceserver2.feign.LocalFeignClient;
+import at.johnny.security.resourceserver2.feign.ResourceServer1FeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,16 @@ public class FeignController {
     @Autowired
     private LocalFeignClient localFeignClient;
 
-    @GetMapping("/feign")
-    public String feign(){
-        return localFeignClient.hello();
+    @Autowired
+    private ResourceServer1FeignClient resourceServer1FeignClient;
+
+    @GetMapping("/hello1")
+    public String callLocalHello(){
+        return localFeignClient.callLocalHello();
+    }
+
+    @GetMapping("/hello2")
+    public String callResourceServer1Hello(){
+        return resourceServer1FeignClient.callResourceServer1Hello();
     }
 }
