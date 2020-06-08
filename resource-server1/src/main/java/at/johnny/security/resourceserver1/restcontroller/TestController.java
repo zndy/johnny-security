7d07ce8c68810195b54377c258e91b9324be2818
z.xin <z.xin@WKS-XIN>
2020-06-08 12:43:51 +0200

    adds readme.md

--- resource-server1/src/main/java/at/johnny/security/resourceserver1/restcontroller/TestController.java
+++ resource-server1/src/main/java/at/johnny/security/resourceserver1/restcontroller/TestController.java
@@ -2,7 +2,9 @@ package at.johnny.security.resourceserver1.restcontroller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +13,25 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class TestController {
     @GetMapping("/hello")
-    public String hello(HttpServletRequest request){
+    public String hello(HttpServletRequest request) {
         return "good morning " + request.getLocalAddr();
     }
 
     @GetMapping("/talk")
-    public String talk(){
+    public String talk() {
         return "heute ist windig.";
     }
 
     @GetMapping("/me")
-    public Object getCurrentUser(Authentication user){
+    public Object getCurrentUser(Authentication user) {
         return user;
+    }
+
+    @GetMapping("/mee")
+    public String token() {
+        OAuth2AuthenticationDetails auth = (OAuth2AuthenticationDetails) SecurityContextHolder.getContext().getAuthentication().getDetails();
+        String tokenValue = auth.getTokenValue();
+        return tokenValue;
     }
 
 }
