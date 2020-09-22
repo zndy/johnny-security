bda33743387fd7e6bf52f913cddb8e64140d551d
z.xin <z.xin@WKS-XIN>
2020-09-22 08:53:55 +0200

    //

--- resource-server1/src/main/java/at/johnny/security/resourceserver1/restcontroller/TestController.java
+++ resource-server1/src/main/java/at/johnny/security/resourceserver1/restcontroller/TestController.java
@@ -1,9 +1,7 @@
 package at.johnny.security.resourceserver1.restcontroller;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +21,7 @@ public class TestController {
     }
 
     @GetMapping("/me")
-    public Object getCurrentUser(Authentication user) {
+    public Authentication getCurrentUser(Authentication user) {
         return user;
     }
 
